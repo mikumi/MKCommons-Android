ecbb461d6bc1fca22aea8c9e0ad0782331abaea5
MichaelK <me@michael-kuck.com>
2014-08-07 15:35:13 +0800

    Added ListUtil class

---
+++ src/main/java/com/michael_kuck/mkcommons/ListUtil.java
@@ -0,0 +1,32 @@
+/*
+ * *
+ *  * This file is part of whereisthat-android
+ *  *
+ *  * Unless otherwise stated in a separate LICENSE file for this project
+ *  * or agreed via contract, all rights reserved by the author.
+ *
+ */
+
+package com.michael_kuck.mkcommons;
+
+import java.util.List;
+
+public class ListUtil {
+
+    public static <T> T getFirstItem(final List<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        } else{
+            return list.get(0);
+        }
+    }
+
+    public static <T> T getLastItem(final List<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        } else {
+            return list.get(list.size() - 1);
+        }
+    }
+
+}
