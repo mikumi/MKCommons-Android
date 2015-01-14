73510dffb95d4cf3ac8fae69726dc33f6844b25d
MichaelK <me@michael-kuck.com>
2015-01-14 16:46:23 +0800

    Add class for tracking library version

---
+++ src/main/java/com/michael_kuck/android/mkcommons/Version.java
@@ -0,0 +1,16 @@
+/*
+ * This file is part of whereisthat-android
+ *
+ * Unless otherwise stated in a separate LICENSE file for this project
+ * or agreed via contract, all rights reserved by the author.
+ */
+
+package com.michael_kuck.android.mkcommons;
+
+public class Version {
+
+    final public static int MAJOR = 1;
+    final public static int MINOR = 0;
+    final public static int PATCH = 0;
+
+}
